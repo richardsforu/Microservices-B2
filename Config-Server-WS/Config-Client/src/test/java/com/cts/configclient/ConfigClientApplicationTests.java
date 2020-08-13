35c0fe39c212d6472d3667396fb6cd7243d2ab69
richardsforu <praveen.somireddy@gmail.com>
2020-08-13 15:18:05 +0530

    config-server

---
+++ Config-Server-WS/Config-Client/src/test/java/com/cts/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
