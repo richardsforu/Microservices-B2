20a67171e290a66272d1c6eb9bdd23545f23e383
richardsforu <praveen.somireddy@gmail.com>
2020-08-17 15:36:29 +0530

    cloud-bus

--- CloudBus-WS/2-FlightSearch-Service/src/main/java/com/cts/brownfield/pss/Application.java
+++ CloudBus-WS/2-FlightSearch-Service/src/main/java/com/cts/brownfield/pss/Application.java
@@ -2,11 +2,8 @@ package com.cts.brownfield.pss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-//@PropertySources(value = {@PropertySource("b1.p"),@PropertySource("b2.p")})
 public class Application {
 
 	public static void main(String[] args) {
